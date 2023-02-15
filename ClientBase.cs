9a2086c2281745c322895d908e970ce7514ac92a
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-02-15 11:26:29 +0800

    去耦JSON套件，api_url參數

--- ClientBase.cs
+++ ClientBase.cs
@@ -1,51 +1,30 @@
 ﻿using System.Net;
 
 using RestSharp;
-using Newtonsoft.Json;
 
 namespace OpenAPI.Client
 {
-    abstract public class ClientBase
+    public class ClientBase
     {
-        abstract protected string API_URL { get; }
-        protected string Execute(RestRequest request)
+        protected string Execute(string api_url, RestRequest request)
         {
-            using (RestClient client = new RestClient(API_URL))
+            using (RestClient client = new RestClient(api_url))
             {
                 var response = client.Execute(request);
+
                 if (response.StatusCode != HttpStatusCode.OK)
                 {
-                    //優先拋Content
-                    if (response.Content != "")
-                    {
-                        throw new Exception(response.Content);
-                    }
+                    var ErrorException_Message = response.ErrorException.Message;
+                    var Content = response.Content;
 
-                    //沒Content再拋ErrorException
-                    if (response.ErrorException is not null)
-                    {
-                        throw new Exception(response.ErrorException.Message);
-                    }
-
-                    //都沒有就拋StatusDescription
-                    throw new Exception(response.StatusDescription);
-                }
-
-                if (response.Content is null)
-                {
-                    throw new Exception("response.Content is null");
+                    throw new Exception($@"
+ErrorException_Message：{ErrorException_Message}
+Content：{Content}
+");
                 }
 
                 return response.Content;
             }
-        }
-        protected T ExecuteJsonConvert<T>(RestRequest request)
-        {
-            var json = Execute(request);
-            var result = JsonConvert.DeserializeObject<T>(json);
-            if (result is null) throw new Exception("result is null");
-
-            return result;
         }
     }
 }
